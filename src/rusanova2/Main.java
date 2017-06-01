abab9a5ea5c7e750530812fe41e0e2fdd5918f55
NikichXP <nikichx2@gmail.com>
2017-06-01 04:59:14 +0300

    lab 3 & 5

--- src/rusanova2/Main.java
+++ src/rusanova2/Main.java
@@ -12,17 +12,56 @@ public class Main {
 		
 		createLinks();
 //		System.out.println(checkLinks());
-		System.out.println("Cores are linked: " + checkLinks());
+//		System.out.println("Cores are linked: " + checkLinks());
 		
-		createTasks2(5, 1, 5, 0.2, 10, 30);//10, 1, 5, 0.4
-		System.out.println("Tasks are uncycled: " + checkTasks());
-		if (checkTasks()) {
-			printLinks();
+		createTasks2(30, 1, 3, 0.65, 1, 3);//10, 1, 5, 0.4
+//		System.out.println("Tasks are uncycled: " + checkTasks());
+		printLinks();
+//		findCyclesByWarshell();
+		
+		//var 3
+		System.out.println("\nVariant 3: \n");
+		tasks.stream().sorted(Comparator.comparingInt(t -> -t.getCriticalTime())).forEach(System.out::println);
+		
+		
+		//var 5
+		System.out.println("\nVariant 5: \n");
+		getVar5();
+	}
+	
+	private static void getVar5 () {
+		List<Task> crits = tasks.stream() //TODO Fix here
+				.max(Comparator.comparingInt(Task::getCriticalTime))
+				.orElseThrow(() -> new IllegalArgumentException("No routes found"))
+				.getCriticalRoute();
+		
+		System.out.println("crit route: " + crits.stream().map(task -> task.id + "").reduce((s1, s2) -> s1 + " -> " + s2).orElse(""));
+		
+		tasks.stream().sorted((task1, task2) -> {
+			if (retain(tasks, task1.next) != retain(tasks, task2.next)) {
+				return 100* (retain(crits, task2.next) - retain(crits, task1.next));
+			}
+			return task2.getCriticalTime() - task1.getCriticalTime();
+		}).forEach(System.out::println);
+	}
+	
+	public static int retain (List<Task> tasks, List<Task.Link> links) {
+		int ret = 0;
+		for (Task task : tasks) {
+			for (Task.Link link : links) {
+				if (link.link.id == task.id) {
+					ret++;
+				}
+			}
 		}
-		
+		return ret;
 	}
 	
 	private static void printLinks () {
+		if (!checkTasks()) {
+			System.out.println("Tasks are cycled, can not print");
+			return;
+		}
 		boolean[] startNodes = new boolean[tasks.size()];
 		tasks.forEach(task -> task.next.forEach(
 				link -> {
@@ -37,6 +76,7 @@ public class Main {
 			}
 		}
 	}
+	
 	private static String printRoute (LinkedList<Task> list) {
 		/*
 			.map(task -> task.getId() + " (" + task.getWeight() + ")")
@@ -45,11 +85,12 @@ public class Main {
 		 */
 		StringBuilder sb = new StringBuilder(list.get(0).print());
 		for (int i = 0; i < list.size() - 1; i++) {
-			Task.Link link = list.get(i).findNext(list.get(i+1));
+			Task.Link link = list.get(i).findNext(list.get(i + 1));
 			sb.append(" -").append(link.weight).append("-> ").append(link.link.print());
 		}
 		return sb.toString();
 	}
+	
 	private static LinkedList<LinkedList<Task>> getRoutes (Task task) {
 		LinkedList<LinkedList<Task>> ret = new LinkedList<>();
 		if (task.next.size() == 0) {
@@ -65,12 +106,13 @@ public class Main {
 			return ret;
 		}
 	}
-	private static void createTasks2(int tasksCount, int minWegth, int maxWeight, double corellation, int minLink, int maxLink) { //corellation = nodes / (nodes + links)
+	
+	private static void createTasks2 (int tasksCount, int minWegth, int maxWeight, double corellation, int minLink, int maxLink) { //corellation = nodes / (nodes + links)
 		
 		for (int i = 0; i < tasksCount; i++) {
 			tasks.add(new Task(rand.nextInt(maxWeight - minWegth) + minWegth));
 		}
-
+		
 		int from = 0, to = 0;
 		
 		while (calculateCorrelation() > corellation) {
@@ -84,16 +126,17 @@ public class Main {
 			if (!checkTasks()) {
 				tasks.get(from).remove(tasks.get(to));
 				System.out.println("error cycle link " + from + " -> " + to);
-				System.gc();
 			}
-
-
+			
+			
 		}
 		while (calculateCorrelation() < corellation) {
+			System.out.println("here");
 			tasks.get(from).addNext(tasks.get(to), -1);
 		}
 		
 	}
+	
 	private static double calculateCorrelation () {
 		
 		int linkSum = 0, taskSum = 0;
@@ -116,7 +159,7 @@ public class Main {
 		tasks.add(new Task(2));
 		tasks.add(new Task(1));
 //		}
-
+		
 		nextTask(0, 1);
 		nextTask(0, 3);
 		nextTask(1, 2);
@@ -125,13 +168,13 @@ public class Main {
 		nextTask(1, 4);
 
 
-
 //		for (int i = 0; i < 9; i++) {
 //			nextTask(i, i + 1);
 //		}
 //		nextTask(9, 0);
 		
 	}
+	
 	private static boolean checkTasks () {
 		boolean res = true;
 		for (Task task : tasks) {
@@ -139,6 +182,36 @@ public class Main {
 		}
 		return res;
 	}
+	
+	private static boolean findCyclesByWarshell () {
+		int steps[][] = new int[tasks.size()][tasks.size()];
+		for (int i = 0; i < tasks.size(); i++)
+			for (int j = 0; j < tasks.size(); j++)
+				steps[i][j] = 0;
+		
+		tasks.forEach(task -> task.next.forEach(link ->
+				steps[task.id][link.link.id] = 1
+		));
+		for (int i = 0; i < tasks.size(); i++) {
+			for (int j = 0; j < tasks.size(); j++) {
+				for (int k = 0; k < tasks.size(); k++) {
+					if (steps[i][j] != 0 && steps[j][k] != 0) {
+						steps[j][k] = steps[i][j] + steps[j][k];
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < tasks.size(); i++) {
+			System.out.println();
+			for (int j = 0; j < tasks.size(); j++) {
+				System.out.print((steps[i][j] < 1000) ? steps[i][j] + "\t" : "*\t");
+			}
+		}
+		
+		return false;
+	}
+	
 	private static boolean innerRecursive (Task cur, int step) {
 		if (step > tasks.size()) {
 			return false;
@@ -149,11 +222,12 @@ public class Main {
 		}
 		return flag;
 	}
+	
 	private static void createLinks () {
 		for (int i = 0; i < 4; i++) {
 			cores.add(new Core());
 		}
-
+		
 		link(0, 2);
 		link(1, 3);
 //		link (2, 3);
@@ -181,9 +255,11 @@ public class Main {
 		}
 		return x;
 	}
+	
 	private static void link (int a, int b) {
 		cores.get(a).addLink(cores.get(b));
 	}
+	
 	private static void nextTask (int a, int b) {
 		tasks.get(a).addNext(tasks.get(b));
 	}
--- src/rusanova2/Task.java
+++ src/rusanova2/Task.java
@@ -3,6 +3,7 @@ package rusanova2;
 import lombok.Data;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -16,7 +17,6 @@ public class Task {
 	public Task () {
 		this.id = id_gen++;
 	}
-	
 	public Task (int weight) {
 		this();
 		this.weight = weight;
@@ -28,12 +28,11 @@ public class Task {
 		}
 		next.add(new Link(task));
 	}
-	
 	public void addNext (Task task, int weight) {
 		if (weight == 0) {
 			return;
 		}
-		if (task == this) {
+		if (task.id == this.id) {
 			return;
 		}
 		if (next.stream().filter(link -> link.link.id == task.id).findAny().orElse(null) == null) {
@@ -44,23 +43,38 @@ public class Task {
 			next.stream().filter(link -> link.link.id == task.id).findAny().ifPresent(link -> link.weight += weight);
 		}
 	}
-	
 	public void remove (Task task) {
 		next.removeIf(link -> link.link.id == task.id);
 	}
 	
-	public String toString() {
-		return "{ id : " + id + ", weight : " + weight + ", links : [" + next.stream().map(to -> to.link.getId() + "").reduce((l1, l2) -> l1 + ", " + l2).orElse("") + "]}";
+	public int getCriticalTime() {
+		if (next.size() == 0) {
+			return weight;
+		}
+		Task crit = next.stream().max(Comparator.comparingInt(t -> t.link.getCriticalTime())).orElse(null).link;
+		return crit.weight + weight;
 	}
 	
-	public String print () {
-		return getId() + " (" + getWeight() + ")";
+	public List<Task> getCriticalRoute() {
+		if (next.size() == 0) {
+			List<Task> task = new ArrayList<>();
+			task.add(this);
+			return task;
+		}
+		return next.stream().map(task -> task.link.getCriticalRoute()).max(Comparator.comparingInt(List::size)).orElse(new ArrayList<>());
 	}
 	
 	public Link findNext (Task task) {
 		return next.stream().filter(e -> e.link.id == task.id).findAny().orElse(null);
 	}
+	public String print () {
+		return getId() + " (" + getWeight() + ")";
+	}
 	
+	
+	public String toString() {
+		return "{ id : " + id + ", weight : " + weight + ", crittime : " + getCriticalTime() + " ,links : [" + next.stream().map(to -> to.link.getId() + "").reduce((l1, l2) -> l1 + ", " + l2).orElse("") + "]}";
+	}
 	
 	public static class Link {
 		Task link;
